89fd925bb4256844ec7ddf2e3860dbc593d1ed8a
BernardoGSch-prog <127693607+BernardoGSch-prog@users.noreply.github.com>
2023-09-12 19:36:03 +0000

    todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,82 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [todoList, setTodoList] = React.useState([])
+    const [inputValue, setInputValue] = React.useState("")
+    const API_URL = "https://playground.4geeks.com/apis/fake/todos/user/BernardoGSch-prog"
+    const handleAddNewTodo = () => {
+        setTodoList(prev => [...prev, { label: inputValue, done: false }])
+    }
+    const handleDeleteTodo = (indexToRemove) => {
+        setTodoList(prev => prev.filter((_, index) => index !== indexToRemove));
+      }
+    const handleOnKeyDown = (e) => {
+        const keyPressed = e.key
+        if (inputValue && keyPressed === "Enter") {
+            handleAddNewTodo();
+            setInputValue("");
+        }
+    }
+    const handleDeleteAll = () => {
+		
+		fetch('/api/todos/user/BernardoGSch-prog', {
+		  method: 'DELETE',
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		})
+		  .then((response) => {
+			if (response.ok) {
+			  
+			  setTodoList([]);
+			} else {
+			  
+			  console.error('Error al eliminar todos los elementos.');
+			}
+		  })
+		  .catch((error) => {
+			console.error('Error al realizar la solicitud:', error);
+		  });
+	  }
+	  
+    React.useEffect(() => {
+        fetch(API_URL)
+            .then(promiseResponse => {
+                if (promiseResponse.ok)
+                    return promiseResponse.json().then(data => setTodoList(data))
+                const createUserOptions = {
+                    method: "POST",
+                    body: JSON.stringify([]),
+                    headers: { "Content-Type": "application/json" }
+                }
+                fetch(API_URL, createUserOptions)
+            })
+    }, [])
+    React.useEffect(() => {
+        if (todoList.length > 0) {
+            const udpateTodoOptions = {
+                method: "PUT",
+                body: JSON.stringify(todoList),
+                headers: { "Content-Type": "application/json" }
+            }
+            fetch(API_URL, udpateTodoOptions)
+        }
+    }, [todoList])
+    return (
+        <div className="text-center bg-light mx-3 shadow-sm">
+            <h1 className="mt-3">TodoList</h1>
+            <input className="border w-100  text-center" placeholder="Escriba su Tarea" onKeyDown={handleOnKeyDown} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <ul style={{ margin: "1rem" }}>
+                {
+                    todoList.map((singleTodo, index) => {
+                        return <section key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+                            <li >{singleTodo.label}</li>
+                            <button className="btn" onClick={() => handleDeleteTodo(index)}>{`X`}</button>
+                        </section>
+                    })
+                }
+            </ul>
+            <button className="btn" onClick={handleDeleteAll} >Eliminar</button>
+        </div>
+    );
 };
-
 export default Home;
